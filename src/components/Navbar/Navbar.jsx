0a903d8275f29f5af4ec8ed2cf1c9284671fab77
Nikitavlasenko81 <69199501+Nikitavlasenko81@users.noreply.github.com>
2020-12-10 21:46:33 +0200

    ClassComponent

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ function Navbar(){
         <nav className={styles.nav}>
           <div><NavLink to="/profile" activeClassName={styles.active}>Profile</NavLink></div>
           <div><NavLink to="/dialogs" activeClassName={styles.active}>Messages</NavLink></div>
+            <div><NavLink to="/users" activeClassName={styles.active}>Friends</NavLink></div>
           <div><NavLink to="/news" activeClassName={styles.active}>News</NavLink></div>
           <div><NavLink to="/music" activeClassName={styles.active}>Music</NavLink></div>
           <div><NavLink to="/setting" activeClassName={styles.active}>Setting</NavLink></div>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -7,53 +7,61 @@ import defoultPhoto from "../../assets/images/defoultUser.png"
 import styles from "./Users.module.css"
 import Image from "react-bootstrap/Image";
 
-const Users = (props) => {
-    if(props.users.length === 0){
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response =>{
-                props.setUsers(response.data.items);
-            });
-    }
 
-    return(
-        <Row className={"m-4"}>
-            {
-                props.users.map(user => {
-                    return(
-                        <Col sm={4}>
-                            <Card className={"mb-4"}>
-                                <Row>
-                                    <Col className = {styles.avatar}>
-                                        <Image src={user.photos.small !== null ? user.photos.small : defoultPhoto} thumbnail />
-                                    </Col>
-                                </Row>
+ class Users extends React.Component{
+    getUsers = () =>{
+     if( this.props.users.length === 0){
+     axios.get("https://social-network.samuraijs.com/api/1.0/users")
+ .then(response =>{
+     this.props.setUsers(response.data.items);
+ });
+}
+ }
+    render() {
+        return(
+            <div>
+                <Button onClick = {this.getUsers}>GetUsers</Button>
+            <Row className={"m-4"}>
+                {
+                    this.props.users.map(user => {
+                        return(
+                            <Col sm={4}>
+                                <Card className={"mb-4"}>
                                     <Row>
-                                <Card.Body>
-                                    <Card.Title>{user.name}</Card.Title>
-                                    <Card.Text>
-                                        {user.status}
-                                    </Card.Text>
-                                    <Row>
-{/*                                        <Col md={7}>
-                                            <p className = {"mb-0"}>{user.location.city} {user.location.country}</p>
-                                        </Col>*/}
-                                        <Col md={4}>
-                                            {user.followed
-                                                ? <Button onClick = {()=>{props.unfollow(user.id)} } variant="light">Follow</Button>
-                                                : <Button onClick = {()=>{props.follow(user.id)} } variant="light">Unfollow</Button>}
+                                        <Col className = {styles.avatar}>
+                                            <Image src={user.photos.small !== null ? user.photos.small : defoultPhoto} thumbnail />
                                         </Col>
                                     </Row>
-                                </Card.Body>
+                                    <Row>
+                                        <Card.Body>
+                                            <Card.Title>{user.name}</Card.Title>
+                                            <Card.Text>
+                                                {user.status}
+                                            </Card.Text>
+                                            <Row>
+                                                {/*                                        <Col md={7}>
+                                            <p className = {"mb-0"}>{user.location.city} {user.location.country}</p>
+                                        </Col>*/}
+                                                <Col md={4}>
+                                                    {user.followed
+                                                        ? <Button onClick = {()=>{ this.props.unfollow(user.id)} } variant="light">Follow</Button>
+                                                        : <Button onClick = {()=>{ this.props.follow(user.id)} } variant="light">Unfollow</Button>}
+                                                </Col>
+                                            </Row>
+                                        </Card.Body>
                                     </Row>
-                                <Card.Footer>
-                                    <small className="text-muted">Last updated 3 mins ago</small>
-                                </Card.Footer>
-                            </Card>
-                        </Col>
-                    )
-                })
-            }
-        </Row>
+                                    <Card.Footer>
+                                        <small className="text-muted">Last updated 3 mins ago</small>
+                                    </Card.Footer>
+                                </Card>
+                            </Col>
+                        )
+                    })
+                }
+            </Row>
+            </div>
         )
-}
+    }
+
+ }
 export default Users;
